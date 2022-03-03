4faa971862b71c4dfb4d42cc47066a251039b932
dietervansteenwegen <github@vansteenwegen.org>
2022-03-03 13:55:43 +0100

    tagging for new version v0.1.1-beta

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ project = 'Panthyr FLIR PTU-D48e'
 author = 'Dieter Vansteenwegen'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0-beta'
+release = '0.1.1-beta'
 
 # -- General configuration ---------------------------------------------------
 
--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@
 from setuptools import find_packages
 from setuptools import setup
 
-setup(name='p_flir_ptu_d48e', version='0.1.0-beta', packages=find_packages())
+setup(name='p_flir_ptu_d48e', version='0.1.1-beta', packages=find_packages())
