c67150f0f1711dc0fd348527666fee3c663533a7
dietervansteenwegen <github@vansteenwegen.org>
2022-03-03 14:00:46 +0100

    Update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ project = 'Panthyr FLIR PTU-D48e'
 author = 'Dieter Vansteenwegen'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1-beta'
+release = '0.2.0'
 
 # -- General configuration ---------------------------------------------------
 
